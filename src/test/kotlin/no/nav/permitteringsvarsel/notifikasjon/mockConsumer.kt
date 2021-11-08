9af9d9f21e4167b8ee8cfd8beaa8bc36031c5014
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-11-08 13:41:48 +0100

    lagt til mock for kafkakonsumer

--- src/test/kotlin/no/nav/permitteringsvarsel/notifikasjon/mockConsumer.kt
+++ src/test/kotlin/no/nav/permitteringsvarsel/notifikasjon/mockConsumer.kt
@@ -1,14 +1,19 @@
 package no.nav.permitteringsvarsel.notifikasjon
 
+import kotlinx.coroutines.delay
 import no.nav.permitteringsvarsel.notifikasjon.kafka.permitteringsmeldingtopic
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.MockConsumer
 import org.apache.kafka.clients.consumer.OffsetResetStrategy
 import org.apache.kafka.common.TopicPartition
+import java.util.*
 
 val topic = TopicPartition(permitteringsmeldingtopic, 0)
 
 fun mockConsumer() = MockConsumer<String, String>(OffsetResetStrategy.EARLIEST).apply {
     schedulePollTask {
         rebalance(listOf(topic))
+        updateBeginningOffsets(mapOf(Pair(topic, 0)))
+        addRecord(ConsumerRecord(permitteringsmeldingtopic, 0, 0, UUID.randomUUID().toString(), "lokal melding"))
     }
 }
